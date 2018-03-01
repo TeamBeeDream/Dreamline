c3e78913c106465cfbbd25e85593ef1002be2e83
NathanTaylorHunt <nathan.taylor.hunt@gmail.com>
2018-03-01 16:13:37 -0500

    cleanup

--- code/src/config/config.h
+++ code/src/config/config.h
@@ -1,6 +1,15 @@
 #ifndef config_h
 #define config_h
 
-#define DEV
+#ifdef __APPLE__
+    #include "TargetConditionals.h"
+    #if TARGET_OS_IPHONE
+    #elif TARGET_IPHONE_SIMULATOR
+    #elif TARGET_OS_MAC
+        #define DEV
+    #endif
+#else
+    #error "Unsupported platform"
+#endif
 
 #endif

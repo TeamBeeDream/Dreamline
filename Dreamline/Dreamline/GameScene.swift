7644c53453938127d665deec3220b4d8570dad50
NathanTaylorHunt <nathan.taylor.hunt@gmail.com>
2018-03-05 18:15:16 -0500

    new empty xcode project with spritekit

---
+++ Dreamline/Dreamline/GameScene.swift
@@ -0,0 +1,27 @@
+//
+//  GameScene.swift
+//  Dreamline
+//
+//  Created by BeeDream on 3/5/18.
+//  Copyright © 2018 Team BeeDream. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.cyan
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {}
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {}
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {}
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {}
+    
+    override func update(_ currentTime: TimeInterval) {}
+}

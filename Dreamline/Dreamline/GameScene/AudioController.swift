342bb94b12c9047a8a4f765774857a915d7f0401
NathanTaylorHunt <nathan.taylor.hunt@gmail.com>
2018-03-16 15:11:01 -0400

    Add slowdown sound effect

--- Dreamline/Dreamline/GameScene/AudioController.swift
+++ Dreamline/Dreamline/GameScene/AudioController.swift
@@ -16,6 +16,7 @@ class AudioNode: SKNode, AudioController {
     
     private let bingSound: SKAction
     private let powerupSound: SKAction
+    private let slowdownSound: SKAction
     private let deathSound: SKAction
     private let blipSound: SKAction
     
@@ -23,9 +24,11 @@ class AudioNode: SKNode, AudioController {
     private let musicNode: SKAudioNode
     
     override init() {
+        // @TODO: Get resources from resource manager
         // Init all sound actions (equivalent to preloading)
         self.bingSound = SKAction.playSoundFileNamed("Pickup_Coin.wav", waitForCompletion: false)
         self.powerupSound = SKAction.playSoundFileNamed("Powerup.wav", waitForCompletion: false)
+        self.slowdownSound = SKAction.playSoundFileNamed("slowdown_effect.mp3", waitForCompletion: false)
         self.deathSound = SKAction.playSoundFileNamed("Death.wav", waitForCompletion: false)
         self.blipSound = SKAction.playSoundFileNamed("Blip.wav", waitForCompletion: true)
         self.musicNode = SKAudioNode(fileNamed: "dreamline_mainloop_rough.mp3")
@@ -51,6 +54,7 @@ class AudioNode: SKNode, AudioController {
             case .modifierGet(_, let type):
                 switch (type) {
                 case .speedUp: self.playSound(self.powerupSound)
+                case .speedDown: self.playSound(self.slowdownSound)
                 default: break
                 }
             case .barrierHit(_):

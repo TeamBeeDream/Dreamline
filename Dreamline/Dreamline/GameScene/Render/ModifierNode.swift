b82081b4bd62bafa6ef1f0091ab0d95f2cedbaff
NathanTaylorHunt <nathan.taylor.hunt@gmail.com>
2018-03-19 15:41:44 -0400

    Change speed modifier graphics

--- Dreamline/Dreamline/GameScene/Render/ModifierNode.swift
+++ Dreamline/Dreamline/GameScene/Render/ModifierNode.swift
@@ -11,6 +11,8 @@ import SpriteKit
 
 class ModifierNode: SKNode {
     
+    static let graphicWidth: CGFloat = 35.0 // @HARDCODED
+    
     func drawOnce(row: ModifierRow, positions: [CGPoint]) {
         
         for (i, modifier) in row.modifiers.enumerated() {
@@ -18,24 +20,14 @@ class ModifierNode: SKNode {
             case .none:
                 break
             case .speedUp:
-                let node = SKShapeNode(circleOfRadius: 12.0) // @HARDCODED
-                node.fillColor = SKColor.clear
-                node.strokeColor = SKColor(red: 23.0/255.0,
-                                           green: 190.0/255.0,
-                                           blue: 187.0/255.0,
-                                           alpha: 1.0)
-                node.lineWidth = 2.0
+                let node = SKSpriteNode(imageNamed: "SpeedUp")
+                node.size = CGSize(width: ModifierNode.graphicWidth, height: ModifierNode.graphicWidth)
                 node.position.x = positions[i].x
                 self.addChild(node)
                 break
             case .speedDown:
-                let node = SKShapeNode(circleOfRadius: 12.0) // @HARDCODED
-                node.fillColor = SKColor.clear
-                node.strokeColor = SKColor(red: 239.0/255.0,
-                                           green: 62.0/255.0,
-                                           blue: 54.0/255.0,
-                                           alpha: 1.0)
-                node.lineWidth = 2.0
+                let node = SKSpriteNode(imageNamed: "SpeedDown")
+                node.size = CGSize(width: ModifierNode.graphicWidth, height: ModifierNode.graphicWidth)
                 node.position.x = positions[i].x
                 self.addChild(node)
                 break

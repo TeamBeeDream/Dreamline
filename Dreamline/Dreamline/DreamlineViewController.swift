8f6bdc5524fbb30fb9e00066af0cae02d4ebca21
NathanTaylorHunt <nathan.taylor.hunt@gmail.com>
2018-04-27 12:08:45 -0400

    Remove old code

--- Dreamline/Dreamline/DreamlineViewController.swift
+++ Dreamline/Dreamline/DreamlineViewController.swift
@@ -12,8 +12,8 @@ class DreamlineViewController: UIViewController {
     
     // MARK: Private Properties
     
-    private var skView: SKView!
-    private var sceneController: SceneController!
+//    private var skView: SKView!
+//    private var sceneController: SceneController!
     
     // MARK: Init and Deinit
     
@@ -27,18 +27,18 @@ class DreamlineViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.sceneController = DreamlineSceneController.make(size: self.view.frame.size)
-        
-        let skView = SKView(frame: self.view.frame)
-        skView.isMultipleTouchEnabled = true
-        self.view.addSubview(skView)
-        self.skView = skView // @IMPORTANT
-        skView.ignoresSiblingOrder = true
-        skView.showsDrawCount = true
-        skView.showsFPS = true
-        //skView.isAsynchronous = true // @NOTE: I'm not sure what this does
-        
-        self.didTransition(to: .game)
+//        self.sceneController = DreamlineSceneController.make(size: self.view.frame.size)
+//
+//        let skView = SKView(frame: self.view.frame)
+//        skView.isMultipleTouchEnabled = true
+//        self.view.addSubview(skView)
+//        self.skView = skView // @IMPORTANT
+//        skView.ignoresSiblingOrder = true
+//        skView.showsDrawCount = true
+//        skView.showsFPS = true
+//        //skView.isAsynchronous = true // @NOTE: I'm not sure what this does
+//
+//        self.didTransition(to: .game)
     }
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
@@ -51,12 +51,5 @@ class DreamlineViewController: UIViewController {
     
     override var prefersStatusBarHidden: Bool {
         return true
-    }
-}
-
-extension DreamlineViewController: SceneDelegate {
-    func didTransition(to: Scene) {
-        let toScene = self.sceneController.getScene(to, delegate: self)
-        self.skView.presentScene(toScene)
     }
 }

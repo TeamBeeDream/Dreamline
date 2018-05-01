012f7ce220fcd424100e3e72306d7c007a0211a0
NathanTaylorHunt <nathan.taylor.hunt@gmail.com>
2018-05-01 15:44:40 -0400

    Add more sfx

--- Dreamline/Dreamline/Aesthetics/Renderers/AudioRenderer.swift
+++ Dreamline/Dreamline/Aesthetics/Renderers/AudioRenderer.swift
@@ -15,7 +15,11 @@ class AudioRenderer: Observer {
     private var nullNode: SKNode!
     private var backgroundMusic: SKAudioNode!
     private var barrierCross: SKAction!
+    private var barrierPass: SKAction!
     private var thresholdCross: SKAction!
+    private var playerMove: SKAction!
+    
+    private var lane: Int = 0
     
     static func make(scene: SKScene) -> AudioRenderer {
         let instance = AudioRenderer()
@@ -28,6 +32,11 @@ class AudioRenderer: Observer {
         switch event {
         case .settingsMuteUpdate(let mute):
             self.handleMuteUpdate(mute: mute)
+        case .positionTargetUpdate(let target):
+            if lane != target && target != 0 {
+                self.nullNode.run(self.playerMove)
+            }
+            self.lane = target
         case .boardEntityStateUpdate(_, let type, let state):
             self.handleEvent(type: type, state: state)
         case .multiple(let events):
@@ -45,8 +54,6 @@ class AudioRenderer: Observer {
         let musicUrl = Bundle.main.url(forResource: "Dreamline_cloud_theme", withExtension: "mp3")
         self.backgroundMusic = SKAudioNode(url: musicUrl!)
         self.backgroundMusic.autoplayLooped = true
-        self.backgroundMusic.run(SKAction.changeVolume(to: 0.0, duration: 0.0)) // @TEMP
-        self.backgroundMusic.run(SKAction.changeVolume(to: 1.0, duration: 1.0)) // @TEMP
         self.scene.addChild(self.backgroundMusic)
     }
     
@@ -55,7 +62,9 @@ class AudioRenderer: Observer {
         self.nullNode.run(SKAction.changeVolume(to: 0.0, duration: 0.0))
         self.scene.addChild(self.nullNode)
         self.barrierCross = SKAction.playSoundFileNamed("barrier_cross_quiet.mp3", waitForCompletion: false)
+        self.barrierPass = SKAction.playSoundFileNamed("barrier_pass.mp3", waitForCompletion: false)
         self.thresholdCross = SKAction.playSoundFileNamed("threshold_cross.mp3", waitForCompletion: false)
+        self.playerMove = SKAction.playSoundFileNamed("player_move.mp3", waitForCompletion: true)
     }
 
     private func handleEvent(type: EntityType, state: EntityState) {
@@ -65,8 +74,11 @@ class AudioRenderer: Observer {
             default: self.nullNode.run(self.thresholdCross)
             }
         case .barrier:
-            if state == .passed {
+            if state == .crossed {
                 self.nullNode.run(self.barrierCross)
+            }
+            if state == .passed {
+                self.nullNode.run(self.barrierPass)
             }
         default:
             break
@@ -76,6 +88,5 @@ class AudioRenderer: Observer {
     private func handleMuteUpdate(mute: Bool) {
         let volume = Float(mute ? 0.0 : 1.0)
         self.backgroundMusic.run(SKAction.changeVolume(to: volume, duration: 0.5))
-        self.nullNode.run(SKAction.changeVolume(to: volume, duration: 0.5))
     }
 }
